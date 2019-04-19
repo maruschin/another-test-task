3d1550d3f0d51e8bbc4a4a9246c32dc904062a9e
Eugene Maruschin (Evgenii Maruschenko) <maruschin@gmail.com>
2019-04-19 22:08:05 +1000

    Added support for scalars to add and sub methods and made some reorganization

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from typing import Union
 
 
 
@@ -9,22 +10,28 @@ k = 0.5
 
 BasePoint = namedtuple('Point', ('x', 'y'))
 class Point(BasePoint):
-    def add(self, other: 'Point') -> 'Point':
-        return Point(self.x + other.x, self.y + other.y)
+    def add(self, other: Union['Point', int]) -> 'Point':
+        if isinstance(other, Point):
+            return Point(self.x + other.x, self.y + other.y)
+        else:
+            return Point(self.x + other, self.y + other)
 
 
-    def __add__(self, other: 'Point') -> 'Point':
+    def __add__(self, other: Union['Point', int]) -> 'Point':
         return self.add(other)
 
 
-    def sub(self, other: 'Point') -> 'Point':
-        return Point(self.x - other.x, self.y - other.y)
+    def sub(self, other: Union['Point', int]) -> 'Point':
+        if isinstance(other, Point):
+            return Point(self.x - other.x, self.y - other.y)
+        else:
+            return Point(self.x - other, self.y - other)
 
 
-    def __sub__(self, other: 'Point') -> 'Point':
+    def __sub__(self, other: Union['Point', int]) -> 'Point':
         return self.sub(other)
 
 
-    def __eq__(self, other: 'Point') -> bool:
+    def __eq__(self, other: Union['Point', int]) -> bool:
         return (self.x == other.x) and (self.y == other.y)
 
--- tests/test_point.py
+++ tests/test_point.py
@@ -4,21 +4,32 @@ from main import Point
 
 
 A, B = Point(1, 3), Point(3, 1)
+# For equation:
+#   A + B = C
+#   A - B = D
 C, D = Point(4, 4), Point(-2, 2)
+# For equation:
+#   A + 2 = E
+#   A - 2 = F
+E, F = Point(3, 5), Point(-1, 1)
 
 
 def test_add_function():
     assert A.add(B) == C
+    assert A.add(2) == E
 
 
 def test_sub_function():
     assert A.sub(B) == D
+    assert A.sub(2) == F
 
 
 def test_add_operator():
     assert A + B == C
+    assert A + 2 == E
 
 
 def test_sub_operator():
     assert A - B == D
+    assert A - 2 == F
 
