eae7cc74a52e81a83e9328ccbb9df8f8cb352eea
Eugene Maruschin (Evgenii Maruschenko) <maruschin@gmail.com>
2019-04-22 09:07:59 +1000

    Ы

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='anothertt',
-    version='0.0.1',
+    version='0.0.3',
     description='None',
     url='http://github.com/maruschin',
     author='Evgenii Marushchenko',
     author_email='maruschin@gmail.com',
     license='Unlicense',
-    packages=['app'],
+    packages=['anothertt'],
+    libraries=['pillow'],
     zip_safe=False,
 )
 
